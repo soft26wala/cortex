b997aeef35ab5d6f2b3aa1abef189fb8d75d639b
soft26wala <soft26wabdev@gmail.com>
2025-12-10 09:00:49 +0530

    first commit6

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,29 @@
 /** @type {import('next').NextConfig} */
+const basePath = "";
+
 const nextConfig = {
   reactStrictMode: true,
+
+  // REMOVE STATIC EXPORT (VERY IMPORTANT)
+  // output: "export",  <-- REMOVE THIS LINE
+
+  basePath: basePath || undefined,
+  assetPrefix: basePath || undefined,
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cortex-api-htc8.onrender.com",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
+        port: "",
+        pathname: "/uploads/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
