58b0ee59ccddb87f18df6b433dd1359c48ac559f
soft26wala <soft26wabdev@gmail.com>
2025-12-10 08:43:17 +0530

    first commit4

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,15 @@
 /** @type {import('next').NextConfig} */
-// const basePath = "/symposium-nextjs";
-const basePath = "";
 const nextConfig = {
-	// build static export output
-	output: "export",
-	// ensure exported pages end up as folders (helps GitHub Pages routing)
-	basePath: basePath || undefined,
-	assetPrefix: basePath || undefined,
-    env:{
-        NEXT_PUBLIC_BASE_PATH:basePath
-    },
-    reactStrictMode: true,
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cortex-api-htc8.onrender.com",
-        pathname: "/uploads/**"
-      }
-    ]
-  }
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
-// export default nextConfig;
